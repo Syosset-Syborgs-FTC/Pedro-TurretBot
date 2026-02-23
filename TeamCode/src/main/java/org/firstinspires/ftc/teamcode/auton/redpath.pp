6dd803ae74c8588d49fc82a733ce903cab5d30fc
liujustin604 <liujustin604@gmail.com>
2026-02-23 11:31:47 -0500

    add path file

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/redpath.pp
@@ -0,0 +1,292 @@
+{
+  "startPoint": {
+    "x": 64,
+    "y": 16,
+    "heading": "linear",
+    "startDeg": 180,
+    "endDeg": 90,
+    "locked": false
+  },
+  "lines": [
+    {
+      "id": "line-fe7x9wuu4zj",
+      "name": "Preload",
+      "endPoint": {
+        "x": -17.896670493685406,
+        "y": 19.14121699196329,
+        "heading": "linear",
+        "startDeg": 180,
+        "endDeg": 140
+      },
+      "controlPoints": [
+        {
+          "x": -7.971297359357067,
+          "y": 11.69345579793341
+        }
+      ],
+      "color": "#BACD79",
+      "locked": false,
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": ""
+    },
+    {
+      "id": "mlbajvqc-6ojxhm",
+      "name": "GPP",
+      "endPoint": {
+        "x": 38.9345579793341,
+        "y": 62.57526134924555,
+        "heading": "linear",
+        "reverse": false,
+        "startDeg": 140,
+        "endDeg": 90
+      },
+      "controlPoints": [
+        {
+          "x": 5.045350172215876,
+          "y": 18.257175660160726
+        },
+        {
+          "x": 24.60218140068889,
+          "y": 21.382319173363967
+        },
+        {
+          "x": 42.31859931113663,
+          "y": 19.985074626865696
+        }
+      ],
+      "color": "#56ABB5",
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": ""
+    },
+    {
+      "id": "mlbamvt9-zezs1n",
+      "name": "GPPReturn",
+      "endPoint": {
+        "x": -17.72789896670494,
+        "y": 18.90011481056257,
+        "heading": "linear",
+        "reverse": false,
+        "startDeg": 90,
+        "endDeg": 140
+      },
+      "controlPoints": [
+        {
+          "x": 36.41848450057406,
+          "y": 15.757205875541246
+        }
+      ],
+      "color": "#DBD8C6",
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": ""
+    },
+    {
+      "id": "mlbantxu-1w9vx0",
+      "name": "PGP",
+      "endPoint": {
+        "x": 14.21584385763493,
+        "y": 60.39954075774972,
+        "heading": "linear",
+        "reverse": false,
+        "startDeg": 140,
+        "endDeg": 90
+      },
+      "controlPoints": [
+        {
+          "x": 13.843283582089548,
+          "y": 12.264064293915055
+        },
+        {
+          "x": 15.504592422502874,
+          "y": 30.730195177956375
+        }
+      ],
+      "color": "#687AA5",
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": ""
+    },
+    {
+      "id": "mlbarqyd-vbe6ea",
+      "name": "PGPReturn",
+      "endPoint": {
+        "x": -17.772675086107924,
+        "y": 18.928877881966557,
+        "heading": "linear",
+        "reverse": false,
+        "startDeg": 90,
+        "endDeg": 140
+      },
+      "controlPoints": [
+        {
+          "x": 10.29047072330657,
+          "y": 24.486252947871918
+        }
+      ],
+      "color": "#B8B78B",
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": ""
+    },
+    {
+      "id": "mlbau95g-0oywzc",
+      "name": "PPG",
+      "endPoint": {
+        "x": -10.943742824339864,
+        "y": 53.4374282433984,
+        "heading": "linear",
+        "reverse": false,
+        "startDeg": 140,
+        "endDeg": 90
+      },
+      "controlPoints": [
+        {
+          "x": -8.977037887485693,
+          "y": 25.031603120087752
+        }
+      ],
+      "color": "#7D56A9",
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": ""
+    },
+    {
+      "id": "mlbb4s96-f4le3z",
+      "name": "PPGReturn",
+      "endPoint": {
+        "x": -17.407577497129722,
+        "y": 18.936854190585535,
+        "heading": "linear",
+        "reverse": false,
+        "startDeg": 90,
+        "endDeg": 140
+      },
+      "controlPoints": [],
+      "color": "#8DC98A",
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": ""
+    },
+    {
+      "id": "mlbdfovd-f15ezi",
+      "name": "LeaveZone",
+      "endPoint": {
+        "x": 1.9609644087256015,
+        "y": 51.0321469575201,
+        "heading": "linear",
+        "reverse": false,
+        "startDeg": 140,
+        "endDeg": 90
+      },
+      "controlPoints": [],
+      "color": "#AAB8DC",
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": ""
+    }
+  ],
+  "shapes": [
+    {
+      "id": "triangle-1",
+      "name": "Red Goal",
+      "vertices": [
+        {
+          "x": 2,
+          "y": -72
+        },
+        {
+          "x": -72,
+          "y": -72
+        },
+        {
+          "x": -72,
+          "y": -48
+        },
+        {
+          "x": -47,
+          "y": -66
+        },
+        {
+          "x": 2,
+          "y": -66
+        }
+      ],
+      "color": "#dc2626",
+      "fillColor": "#ff6b6b"
+    },
+    {
+      "id": "triangle-2",
+      "name": "Blue Goal",
+      "vertices": [
+        {
+          "x": -47,
+          "y": 66
+        },
+        {
+          "x": -72,
+          "y": 47
+        },
+        {
+          "x": -72,
+          "y": 72
+        },
+        {
+          "x": 2,
+          "y": 72
+        },
+        {
+          "x": 2,
+          "y": 65
+        }
+      ],
+      "color": "#2563eb",
+      "fillColor": "#60a5fa"
+    }
+  ],
+  "sequence": [
+    {
+      "kind": "path",
+      "lineId": "line-fe7x9wuu4zj"
+    },
+    {
+      "kind": "path",
+      "lineId": "mlbajvqc-6ojxhm"
+    },
+    {
+      "kind": "path",
+      "lineId": "mlbamvt9-zezs1n"
+    },
+    {
+      "kind": "path",
+      "lineId": "mlbantxu-1w9vx0"
+    },
+    {
+      "kind": "path",
+      "lineId": "mlbarqyd-vbe6ea"
+    },
+    {
+      "kind": "path",
+      "lineId": "mlbau95g-0oywzc"
+    },
+    {
+      "kind": "path",
+      "lineId": "mlbb4s96-f4le3z"
+    },
+    {
+      "kind": "path",
+      "lineId": "mlbdfovd-f15ezi"
+    }
+  ],
+  "version": "1.2.1",
+  "timestamp": "2026-02-23T16:28:47.485Z"
+}
